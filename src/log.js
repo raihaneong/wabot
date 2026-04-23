22898aeb36c8be26b8e9c9bf123190eb7c579791
rhn <raihneong@gmail.com>
2026-04-24 04:47:35 +0700

    fix: separate log module

---
+++ src/log.js
@@ -0,0 +1,7 @@
+module.exports = {
+  logDate: () => {
+    const d = new Date();
+    const pad = (n) => String(n).padStart(2, "0");
+    return `${d.getFullYear()}-${pad(d.getMonth() + 1)}-${pad(d.getDate())} ${pad(d.getHours())}:${pad(d.getMinutes())}:${pad(d.getSeconds())} -`;
+  },
+};
