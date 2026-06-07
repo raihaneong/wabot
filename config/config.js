836d1b14b767730b568565df4520ccbce6681d21
rhn <raihneong@gmail.com>
2026-06-07 21:41:23 +0700

    config chrome default path

---
+++ config/config.js
@@ -0,0 +1,6 @@
+export default {
+  prefix: ["!", ".", "#", "$", "/"],
+  chromePathWindows:
+    "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe",
+  chromePathLinux: "/usr/bin/google-chrome-stable",
+};
