4163f63dc3e7dc39d653520e6d890543aa769a67
rhn <raihneong@gmail.com>
2026-06-13 02:34:57 +0700

    move to src and delete unnecessary file

--- src/utils/log.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  logDate: () => {
-    const d = new Date();
-    const pad = (n) => String(n).padStart(2, "0");
-    return `${d.getFullYear()}-${pad(d.getMonth() + 1)}-${pad(d.getDate())} ${pad(d.getHours())}:${pad(d.getMinutes())}:${pad(d.getSeconds())} -`;
-  },
-};
