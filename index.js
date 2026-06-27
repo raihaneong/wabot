2ac8679ff59451df7abe8b30714b62cf37592d02
rhn <raihneong@gmail.com>
2026-06-27 12:11:10 +0700

    puppeteer config

--- index.js
+++ index.js
@@ -10,16 +10,16 @@ const client = new Client({
   authStrategy: new LocalAuth({}),
   puppeteer: {
     // headless: true,
-    // args: [
-    //   "--no-sandbox",
-    //   "--disable-setuid-sandbox",
-    //   "--disable-dev-shm-usage",
-    //   "--disable-accelerated-2d-canvas",
-    //   "--no-first-run",
-    //   "--no-zygote",
-    //   // "--single-process",
-    //   "--disable-gpu",
-    // ],
+    args: [
+      "--no-sandbox",
+      "--disable-setuid-sandbox",
+      //   "--disable-dev-shm-usage",
+      //   "--disable-accelerated-2d-canvas",
+      //   "--no-first-run",
+      "--no-zygote",
+      //   "--single-process",
+      //   "--disable-gpu",
+    ],
     // executablePath:
     // "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe",
   },
