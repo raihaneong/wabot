9eb281dc57ede6c7d60721579e18d4ab40fbf1eb
rhn <raihneong@gmail.com>
2026-09-01 19:47:39 +0700

    merge config

--- config/config.js
+++
@@ -1,6 +0,0 @@
-export default {
-  prefix: ["!", ".", "#", "$", "/"],
-  chromePathWindows:
-    "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe",
-  chromePathLinux: "/usr/bin/google-chrome-stable",
-};
